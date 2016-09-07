718076d97243745d7bad40b41048d35ac34aec21
shelbyt <sht005@eng.ucsd.edu>
2016-09-06 20:14:11 -0700

    Extension runs exclusively on youtube WATCH pages
    Uses the older regex parser to determine if a page is some kind of
    youtube page with a watch link. To pick up the URL and pass it through
    the parser, we have to use the async chrome.tabs.query call. This means
    that acting on this we have to put all the logic INTO the tabs query
    callback function.
    
    Close issue #5

--- client/background.js
+++ client/background.js
@@ -8,60 +8,54 @@ function youtubeParser(url) {
     return (match && match[7].length == 11) ? match[7] : false;
 }
 
+function clearStorage() {
+
+    chrome.storage.local.clear(function() {
+        var error = chrome.runtime.lastError;
+        if (error) {
+            console.error(error);
+        }
+    });
+}
+
 chrome.commands.onCommand.addListener(function(command) {
 
-    if (command === "log-time") {
+    chrome.tabs.query({
+            'active': true,
+            'windowId': chrome.windows.WINDOW_ID_CURRENT
+        },
+        function(tabs) {
+            // TODO:(shelbyt): Do we have to  cover an edge case
+            // in case someone tries to use the hotkey on the chome
+            // developer page or some other page like chrome-extension
+            // for which we explictly do NOT have permissions for? Right now
+            // this just throws an exception and nothing happens which is OK.
+            video_url = tabs[0].url;
+            video_id = youtubeParser(video_url);
+            if (typeof(video_id) !== 'undefined' && video_id !== false) {
 
-        chrome.tabs.query({
-                'active': true,
-                'windowId': chrome.windows.WINDOW_ID_CURRENT
-            },
-            function(tabs) {
-                video_url = tabs[0].url;
-                video_id = youtubeParser(video_url);
-            }
-        );
+                var time = [];
+                var stamp;
 
-        //console.log('onCommand event received for message: ', command);
-
-
-
-
-        var time = [];
-        var stamp;
-
-/*
-        chrome.storage.local.clear(function() {
-            var error = chrome.runtime.lastError;
-            if (error) {
-                console.error(error);
-            }
-        });
-	*/
-	
+                var storage = chrome.storage.local;
 
 
+                //console.debug("before post");
+                chrome.tabs.executeScript(null, {
+                        file: "inject.js"
+                    },
+                    function(test) {
+                        var variable = 10;
+                        time.push(test);
+                        console.log(variable);
 
-
-        var storage = chrome.storage.local;
-
-
-        //console.debug("before post");
-        chrome.tabs.executeScript(null, {
-                file: "inject.js"
-            },
-            function(test) {
-                var variable = 10;
-                time.push(test);
-                console.log(variable);
-
-                //console.log(time);
-                //console.log(time[0][0]);
-                stamp = time[0][0];
-                video_time = stamp;
-                //console.log(time.length);
-                chrome.storage.local.get(function(cfg) {
-			/*
+                        //console.log(time);
+                        //console.log(time[0][0]);
+                        stamp = time[0][0];
+                        video_time = stamp;
+                        //console.log(time.length);
+                        chrome.storage.local.get(function(cfg) {
+                            /*
 			var tick_key = cfg[video_id];
                     if (typeof(tick_key) !== 'undefined' &&
                         tick_key["ticks"] instanceof Array) {
@@ -72,86 +66,76 @@ chrome.commands.onCommand.addListener(function(command) {
                     }
 		    */
 
-                    
-                    if (typeof(cfg[video_id]) !== 'undefined' && cfg[video_id] instanceof Array) {
-                        cfg[video_id].push(stamp);
-                    } else {
-                        cfg[video_id] = [stamp];
+
+                            if (typeof(cfg[video_id]) !== 'undefined' && cfg[video_id] instanceof Array) {
+                                cfg[video_id].push(stamp);
+                            } else {
+                                cfg[video_id] = [stamp];
+                            }
+
+                            chrome.storage.local.set(cfg);
+                        });
+                        /*
+                            chrome.storage.sync.set({ "data" : test }, function() {
+                            if (chrome.runtime.error) {
+                              console.log("Runtime error.");
+                            }
+                          });
+                        */
+
+
+                    });
+
+                console.log("before post");
+                jQuery.ajax({
+                    type: "POST",
+                    async: false,
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8",
+                    url: "http://ec2-52-42-224-68.us-west-2.compute.amazonaws.com:8080/weedwizard",
+
+                    data: JSON.stringify({
+                        "tickedAt": video_time,
+                        "videoId": video_id
+                    }),
+                    success: function(data) {
+                        //console.log("inside post");
+                        console.log(data);
+                        //console.debug(data);
                     }
-		    
-                    chrome.storage.local.set(cfg);
                 });
+                console.log("after post");
                 /*
-                    chrome.storage.sync.set({ "data" : test }, function() {
-                    if (chrome.runtime.error) {
-                      console.log("Runtime error.");
+                        storage.get("key", function(result) {
+                            console.log(result);
+                            console.log(result["key"][0])
+
+                        })
+                	*/
+
+
+                function callback(bytes) {
+                    console.log("Total bytes in use: " + bytes);
+                }
+                storage.getBytesInUse(null, callback);
+
+
+                Storage.showTotalBytes = function() {
+                    function callback(bytes) {
+                        console.log("Total bytes in use: " + bytes);
                     }
-                  });
-                */
+                    if (arguments.length == 0) {
+                        console.log("0 arguments");
+                        return chrome.storage.local.getBytesInUse(null, callback);
+                    } else {
 
-
-            });
-
-        console.log("before post");
-        jQuery.ajax({
-            type: "POST",
-            async: false,
-            dataType: "json",
-            contentType:"application/json; charset=utf-8",
-            url: "http://ec2-52-42-224-68.us-west-2.compute.amazonaws.com:8080/weedwizard",
-
-            data: JSON.stringify({
-                "tickedAt": video_time, "videoId": video_id
-            }),
-            success: function(data) {
-                //console.log("inside post");
-                console.log(data);
-                //console.debug(data);
+                        console.log("more arguments");
+                        var ary = arguments.slice();
+                        ary.push(callback);
+                        return chrome.storage.local.getBytesInUse.apply(null, ary);
+                    }
+                };
             }
-        });
-        console.log("after post");
-        /*
-                storage.get("key", function(result) {
-                    console.log(result);
-                    console.log(result["key"][0])
-
-                })
-        	*/
-
-
-        function callback(bytes) {
-            console.log("Total bytes in use: " + bytes);
         }
-        storage.getBytesInUse(null, callback);
-
-
-        Storage.showTotalBytes = function() {
-            function callback(bytes) {
-                console.log("Total bytes in use: " + bytes);
-            }
-            if (arguments.length == 0) {
-                console.log("0 arguments");
-                return chrome.storage.local.getBytesInUse(null, callback);
-            } else {
-
-                console.log("more arguments");
-                var ary = arguments.slice();
-                ary.push(callback);
-                return chrome.storage.local.getBytesInUse.apply(null, ary);
-            }
-        };
-
-
-        //storage.get('myTestVar',function(result){
-        //console.log(result);
-        //console output = {myTestVar:'my test var'}
-        //})
-
-
-
-
-
-
-
-    }
+    );
 });
