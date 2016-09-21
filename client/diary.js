14849d8d962ba717f509c95ad4e8a9c5a41223b7
shelbyt <sht005@eng.ucsd.edu>
2016-09-21 11:45:32 -0700

    Automatically populate new page with video+notes
    First pass of adding content to a new page after it has been
    bookmarked. Take content from chrome storage and fill page
    with title notes and embedded video.

--- client/diary.js
+++ client/diary.js
@@ -2,12 +2,48 @@ function populateSidebar(all_keys, storage) {
 	sidebarBody = document.getElementById("sideBarContent");
 	for (var i = 0; i < all_keys.length; i++) {
 		sideVideoName = document.createElement("a");
-		sideVideoName.href = "#";
+		sideVideoName.href = "#" + all_keys[i].toString();
 		var link_text = document.createTextNode(storage[all_keys[i]].video_name);
 		sideVideoName.appendChild(link_text);
-		sidebarBody.appendChild(sideVideoName);
-	}
+		sidebarBody.appendChild(sideVideoName); }
 }	
+
+function populatePage(all_keys, storage) {
+	frontBody = document.getElementById("main");
+	for(var i = 0; i < all_keys.length; i++) {
+		var title = document.createElement("h1");
+		title.id = all_keys[i].toString();
+		title_text = document.createTextNode(storage[all_keys[i]].video_name);
+		title.appendChild(title_text);
+
+		for(var j = 0; j < storage[all_keys[i]].ticks.length; j++) {
+
+			var note = document.createElement("p");
+			if(storage[all_keys[i]].notes[j]) {
+				note_text = document.createTextNode(
+					storage[all_keys[i]].notes[j]);
+			}
+			else {
+				note_text = document.createTextNode("Notes unavailable");
+			}
+			note.appendChild(note_text);
+			title.appendChild(note);
+
+			var embed_video = document.createElement("iframe");
+			youtube_base = "https://www.youtube.com/embed/"
+			youtube_time = "?start="
+			embed_video.width = "560";
+			embed_video.height = "315";
+			embed_video.frameBorder = "0";
+			embed_video.src = youtube_base + all_keys[i].toString() +
+				youtube_time + 
+				storage[all_keys[i]].ticks[j].toFixed(0).toString();
+			title.appendChild(embed_video);
+		}
+		frontBody.appendChild(title);
+	}
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
 	var storage = chrome.storage.local;
@@ -15,6 +51,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		console.log(result);
 		var allKeys = Object.keys(result);
 		populateSidebar(allKeys, result);
+		populatePage(allKeys, result);
 	});
 
 	document.getElementById("navButtonOpen").addEventListener('click',function() {
