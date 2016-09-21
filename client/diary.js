e5f3c5ba8c4ff466e8cc8a035855d5129afb8f10
shelbyt <sht005@eng.ucsd.edu>
2016-09-21 09:02:57 -0700

    Populate diary with video titles
    Filled the html diary sidebar with the titles of the
    videos by appending <a> elements to the sideBar
    parent.

--- client/diary.js
+++ client/diary.js
@@ -1,4 +1,21 @@
+function populateSidebar(all_keys, storage) {
+	sidebarBody = document.getElementById("sideBarContent");
+	for (var i = 0; i < all_keys.length; i++) {
+		sideVideoName = document.createElement("a");
+		sideVideoName.href = "#";
+		var link_text = document.createTextNode(storage[all_keys[i]].video_name);
+		sideVideoName.appendChild(link_text);
+		sidebarBody.appendChild(sideVideoName);
+	}
+}	
+
 document.addEventListener('DOMContentLoaded', function() {
+	var storage = chrome.storage.local;
+	storage.get(null, function(result) {
+		console.log(result);
+		var allKeys = Object.keys(result);
+		populateSidebar(allKeys, result);
+	});
 
 	document.getElementById("navButtonOpen").addEventListener('click',function() {
 		document.getElementById("mySidenav").style.width = "250px";
