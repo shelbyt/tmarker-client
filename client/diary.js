48c1363740534b424081a977450624e5817eab28
shelbyt <sht005@eng.ucsd.edu>
2016-09-20 19:16:46 -0700

    RF+Remove javascript from html
    Refactor javascript and removed void function from the html
    code that was causing an error.

--- client/diary.js
+++ client/diary.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-document.getElementById("navButtonOpen").addEventListener('click',function() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+	document.getElementById("navButtonOpen").addEventListener('click',function() {
+		document.getElementById("mySidenav").style.width = "250px";
+		document.getElementById("main").style.marginLeft = "250px";
 
-document.getElementById("navButtonClose").addEventListener('click',function() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+		document.getElementById("navButtonClose").addEventListener('click',function() {
+			document.getElementById("mySidenav").style.width = "0";
+			document.getElementById("main").style.marginLeft= "0";
+		});
+
+	});
 });
-
-});
-
-
