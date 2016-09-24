c8f84d39fec2d5d8a2bfb8f873942f75c1e3f162
shelbyt <sht005@eng.ucsd.edu>
2016-09-23 17:29:44 -0700

    Remove stray notifications on fast bookmark press
    After the hotkey is pressed check if there is an iframe from
    the last press and remove it if there is. Causes some issue
    though:
    
    VM20616 jquery-3.1.0.min.js:3Blocked a frame with origin
    "chrome-extension://hndokbjgnoiblaepnigalookmlkbpdki" from accessing a frame
    with origin "https://www.youtube.com".  The frame requesting access has a
    protocol of "chrome-extension", the frame being accessed has a protocol of
    "about". Protocols must match.
    
    Close #14

--- client/inject.js
+++ client/inject.js
@@ -1,5 +1,14 @@
 // The FINAL line is returned. If there is a newline after
 // the value that needs to be returned then IT WILL RETURN UNDEFINED
+//
+
+// TODO(shelbyt): Causes some cross domain error.
+// Check if there is a stray iframe left from the last
+// bookmark and remove it if there is.
+if($('#frame_notify').length) {
+$('#frame_notify').remove();
+}
+
 var tube = document.getElementById("watch-header");
 var iFrame  = document.createElement ("iframe");
 iFrame.src  = chrome.extension.getURL ("notify.html");
@@ -11,7 +20,10 @@ tube.insertBefore(iFrame, tube.childNodes[0]);
 
 setTimeout(function(){
 $('#frame_notify').fadeOut(500);
-},1000)
+},500)
+
+
+
 
 var video = document.getElementsByTagName("video")[0];
 video.currentTime
